0068e7eb39baac55506bb429ff0c365953b0c989
tourze <tourze@users.noreply.github.com>
2025-05-29 17:47:36 +0800

    fix(attribute): 更新 SnowflakeColumn 注释说明

--- src/Attribute/SnowflakeColumn.php
+++ src/Attribute/SnowflakeColumn.php
@@ -4,6 +4,8 @@ namespace Tourze\DoctrineSnowflakeBundle\Attribute;
 
 /**
  * 标记字段自动生成雪花算法ID
+ *
+ * 我们不应该在 id 主键上使用这个注解，雪花id主键应使用 `#[ORM\CustomIdGenerator(SnowflakeIdGenerator::class)]`
  */
 #[\Attribute(\Attribute::TARGET_PROPERTY)]
 class SnowflakeColumn
