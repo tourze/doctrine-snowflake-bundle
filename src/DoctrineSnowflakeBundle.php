bf634ebad10dc6d9cdd20e3397ce4c2b21755b78
github-ci <you@example.com>
2025-06-11 11:25:13 +0800

    update

--- src/DoctrineSnowflakeBundle.php
+++ src/DoctrineSnowflakeBundle.php
@@ -2,15 +2,18 @@
 
 namespace Tourze\DoctrineSnowflakeBundle;
 
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BundleDependency\BundleDependencyInterface;
+use Tourze\SnowflakeBundle\SnowflakeBundle;
 
 class DoctrineSnowflakeBundle extends Bundle implements BundleDependencyInterface
 {
     public static function getBundleDependencies(): array
     {
         return [
-            \Tourze\SnowflakeBundle\SnowflakeBundle::class => ['all' => true],
+            DoctrineBundle::class => ['all' => true],
+            SnowflakeBundle::class => ['all' => true],
         ];
     }
 }
